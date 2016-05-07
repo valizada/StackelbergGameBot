5a15edd797f5e250615bb013c41bbcf9bfffa435
valizada <vfizuli@gmail.com>
2016-05-07 02:01:55 +0100

    finished squared function

---
+++ src/SquareLeader.java
@@ -0,0 +1,163 @@
+import comp34120.ex2.PlayerImpl;
+import comp34120.ex2.PlayerType;
+import comp34120.ex2.Record;
+import org.jblas.FloatMatrix;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by fiz on 07/05/2016.
+ */
+
+/**
+ * A very simple leader implementation that only generates random prices
+ * @author Xin
+ */
+final class SquareLeader
+        extends PlayerImpl
+{
+    ArrayList<Record> historicalData;
+    private SquareLeader()
+            throws RemoteException, NotBoundException
+    {
+        super(PlayerType.LEADER, "Square Leader");
+    }
+
+    @Override
+    public void goodbye()
+            throws RemoteException
+    {
+        ExitTask.exit(500);
+    }
+
+    /**
+     * To inform this instance to proceed to a new simulation day
+     * @param p_date The date of the new day
+     * @throws RemoteException
+     */
+    @Override
+    public void proceedNewDay(int p_date)
+            throws RemoteException
+    {
+
+        if (p_date == 101){
+            m_platformStub.publishPrice(m_type, maximiseRecursive(theta));
+//            System.out.println("a: " + theta.get(0) + " b: " + theta.get(1));
+        }
+        else{
+            squareRecursive.addData(m_platformStub.query(PlayerType.LEADER, p_date-1));
+            theta = squareRecursive.update(p_date-1);
+            m_platformStub.publishPrice(m_type, maximiseRecursive(theta));
+        }
+
+        // code for only 100 days
+//		m_platformStub.publishPrice(m_type, (float)maximise(p_date));
+//      m_platformStub.log(m_type, "current date: " + p_date);
+    }
+
+
+    SquareRecursive squareRecursive;
+    FloatMatrix theta;
+    int p_steps = 0;
+    /**
+     * You may want to delete this method if you don't want to do any
+     * initialization
+     * @param p_steps Indicates how many steps will the simulation perform
+     * @throws RemoteException If implemented, the RemoteException *MUST* be
+     * thrown by this method
+     */
+    @Override
+    public void startSimulation(int p_steps)
+            throws RemoteException
+    {
+        super.startSimulation(p_steps);
+        this.p_steps = p_steps;
+
+        historicalData = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++)
+            historicalData.add(m_platformStub.query(PlayerType.LEADER, i));
+
+        squareRecursive = new SquareRecursive(historicalData);
+        theta = squareRecursive.baseCondition();
+    }
+
+    /**
+     * You may want to delete this method if you don't want to do any
+     * finalization
+     * @throws RemoteException If implemented, the RemoteException *MUST* be
+     * thrown by this method
+     */
+    @Override
+    public void endSimulation()
+            throws RemoteException
+    {
+        Record record;
+        float totalProfit = 0;
+
+        for (int i = 1; i <= p_steps; i++){
+            record = m_platformStub.query(PlayerType.LEADER, 100+i);
+
+            totalProfit += calculateProfit(record.m_leaderPrice, record.m_followerPrice);
+        }
+        m_platformStub.log(PlayerType.LEADER, "profit: " + totalProfit);
+    }
+
+    public float maximiseRecursive(FloatMatrix theta) throws RemoteException {
+
+        int count = 0;
+        float max = 0.000f;
+        float currentUl = 0.000f;
+        for (float i = 1.001f; i <= 3.500f; i=i+0.001f) {
+
+            // profit = (Ul - 1) * (2 - Ul + 0.3*Uf)
+            // Uf = a + b * Ul + c * Ul*Ul
+
+            float profit = (float) ((i-1)*(2-i+0.3*(theta.get(0) + theta.get(1)*i + theta.get(2) * i * i)));
+
+            if (profit > max){
+                max = profit;
+                currentUl = i;
+//                System.out.println("current best: " + currentUl);
+            }
+            count++;
+        }
+
+//        System.out.println("looped " + count + " times");
+        System.out.println();
+        return currentUl;
+    }
+
+    public double calculateProfit(float Ul, float Uf){
+        return (Ul - 1) * (2 - Ul + 0.3*Uf);
+    }
+
+    public static void main(final String[] p_args)
+            throws RemoteException, NotBoundException
+    {
+        new SquareLeader();
+    }
+
+    /**
+     * The task used to automatically exit the leader process
+     * @author Xin
+     */
+    private static class ExitTask
+            extends TimerTask
+    {
+        static void exit(final long p_delay)
+        {
+            (new Timer()).schedule(new ExitTask(), p_delay);
+        }
+
+        @Override
+        public void run()
+        {
+            System.exit(0);
+        }
+    }
+}
--- src/SquareRecursive.java
+++ src/SquareRecursive.java
@@ -1,5 +1,4 @@
 import comp34120.ex2.Record;
-import org.jblas.DoubleMatrix;
 import org.jblas.FloatMatrix;
 
 import java.util.ArrayList;
@@ -21,12 +20,11 @@ public class SquareRecursive {
     }
 
     // fogetting factor
-    Float lambda = 0.97f;
+    Float lambda = 0.93f;
     // prediction error
     FloatMatrix P = new FloatMatrix(3,3);
     // parameters
     FloatMatrix theta;
-
 
     public FloatMatrix baseCondition(){
 
@@ -41,37 +39,74 @@ public class SquareRecursive {
             phi.put(2, leaderPrice*leaderPrice);
 
             // todo: datasize-1 part
-            P = P.add(phi.mmul(phi.transpose()).mmul((float) Math.pow(lambda, dataSize-i)));
+            P = P.add(phi.mmul(phi.transpose()).mmul((float) Math.pow(lambda, dataSize-i-1)));
         }
 
         theta = new FloatMatrix(3,1);
-//
-//        DoubleMatrix thetaFollow = new DoubleMatrix(2,1);
-//
-//        for (int i = 0; i < dataSize; i++) {
-//            DoubleMatrix phi = new DoubleMatrix(2,1);
-//
-//            phi.put(0, 1);
-//            phi.put(1,historicalData.get(i).m_leaderPrice);
-//
-//            thetaFollow = thetaFollow.add(phi.mmul(historicalData.get(i).m_followerPrice).mmul(Math.pow(lambda, dataSize-i)));
-//        }
-//
-//        double[][] forInverse = new double[2][2];
-//
-//        forInverse[0][0] = P.get(0);
-//        forInverse[0][1] = P.get(1);
-//        forInverse[1][0] = P.get(2);
-//        forInverse[1][1] = P.get(3);
-//
-//        P.print();
-//        forInverse = Inverse.invert(forInverse);
-//        DoubleMatrix inverseOfP = new DoubleMatrix(forInverse);
-//        inverseOfP.print();
-//        theta = inverseOfP.mmul(thetaFollow);
-//
-        return theta;
 
+        FloatMatrix thetaFollow = new FloatMatrix(3,1);
+
+        for (int i = 0; i < dataSize; i++) {
+            FloatMatrix phi = new FloatMatrix(3,1);
+
+            float leaderPrice = historicalData.get(i).m_leaderPrice;
+            phi.put(0,1);
+            phi.put(1,leaderPrice);
+            phi.put(2, leaderPrice*leaderPrice);
+
+            thetaFollow = thetaFollow.add(phi.mmul(historicalData.get(i).m_followerPrice).mmul((float) Math.pow(lambda, dataSize-i-1)));
+        }
+
+        float[][] forInverse = new float[3][3];
+
+        forInverse[0][0] = P.get(0);
+        forInverse[0][1] = P.get(1);
+        forInverse[0][2] = P.get(2);
+
+        forInverse[1][0] = P.get(3);
+        forInverse[1][1] = P.get(4);
+        forInverse[1][2] = P.get(5);
+
+        forInverse[2][0] = P.get(6);
+        forInverse[2][1] = P.get(7);
+        forInverse[2][2] = P.get(8);
+
+//        P.print();
+        forInverse = Inverse.invert(forInverse);
+        FloatMatrix inverseOfP = new FloatMatrix(forInverse);
+//        inverseOfP.print();
+        theta = inverseOfP.mmul(thetaFollow);
+
+        return theta;
     }
 
+    FloatMatrix adjustingFactor;
+
+    public FloatMatrix update(int date){
+        FloatMatrix phi = new FloatMatrix(3,1);
+
+        float leaderPrice = historicalData.get(date-1).m_leaderPrice;
+
+        phi.put(0, 1);
+        //todo: date -2 must be checked
+        phi.put(1, leaderPrice);
+        phi.put(2, leaderPrice*leaderPrice);
+
+        FloatMatrix adjustingFactorDeter = phi.transpose().mmul(P).mmul(phi).add(lambda);
+        adjustingFactor = P.mmul(phi).div(adjustingFactorDeter);
+
+        FloatMatrix Pnum, Pdenom;
+
+        Pnum = P.mmul(phi).mmul(phi.transpose()).mmul(P);
+
+        Pdenom = phi.transpose().mmul(P).mmul(phi).add(lambda);
+
+        P = P.sub(Pnum.div(Pdenom)).mmul(1/lambda);
+
+        FloatMatrix followerPrice = new FloatMatrix(1,1);
+        //todo: date -2 must be checked
+        followerPrice.put(0, historicalData.get(date-1).m_followerPrice);
+        theta = theta.add(adjustingFactor.mmul((followerPrice.sub(phi.transpose().mmul(theta)))));
+        return theta;
+    }
 }
